997113612c4f3e777bb0b24ffb80e4b753caae52
gumenghao <menghao.gu@leediancn.com>
2017-05-31 18:06:37 +0800

    添加sharedprefencesUtils和stringutils,删除无用接口

--- app/src/main/java/com/homechart/app/commont/UrlConstants.java
+++ app/src/main/java/com/homechart/app/commont/UrlConstants.java
@@ -8,25 +8,9 @@ package com.homechart.app.commont;
  * @brief url地址整合 .
  */
 public class UrlConstants {
-
-    public static final String HOME_PIC_URL = "http://sjj.idcool.com.cn/photo/slist";
-    public static final String HOME_TAB_URL = "http://sjj.idcool.com.cn/filter";
-    public static final String DETAILS_CASE_TITAL = "http://sjj.idcool.com.cn/album/getAlbumDetail";
-    public static final String DETAILS_CASE_PIC = "http://sjj.idcool.com.cn/item/getItemList";
-    public static final String DETAILS_CENTER_TITAL = "http://sjj.idcool.com.cn/user/getUserInfo";
-    public static final String DETAILS_CENTER_PIC = "http://sjj.idcool.com.cn/album/getAlbumList";
-    public static final String DETAILS_CITYS = "http://sjj.idcool.com.cn/filter";
-    public static final String DETAILS_LISTS = "http://sjj.idcool.com.cn/profession/slist";
+    //老接口
     public static final String JIYAN_PHONE_SENDNUM = "http://sjj.idcool.com.cn/account/checkLimit";
     public static final String JIYAN_SEND_MESSAGE = "http://sjj.idcool.com.cn/account/sendCode";
-    public static final String REGISTER_PERSION = "http://sjj.idcool.com.cn/account/register";
-    public static final String REGISTER_CHECKCODE = "http://sjj.idcool.com.cn/account/checkCode";
-    public static final String LOGIN_PERSION = "http://sjj.idcool.com.cn/account/login";
-    public static final String LOGIN_PLAT = "http://sjj.idcool.com.cn/account/connect";
-    public static final String BUNDLE_BEFORE = "http://sjj.idcool.com.cn/account/bind";
-    public static final String PUT_FILE = "http://sjj.idcool.com.cn/upload/uploadImgFile";
-    public static final String PUT_ISSUE = "http://sjj.idcool.com.cn/feed/addFeed";
-    public static final String CHAT_LIST = "http://sjj.idcool.com.cn/im/userContactList";
 
     //新接口
     public static final String USER_LOGIN = "https://api.idcool.com.cn/user/account/login";
