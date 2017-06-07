2b39a2367d5c12ac8d062ec61987e22104a5f75f
gumenghao <menghao.gu@leediancn.com>
2017-06-08 00:34:18 +0800

    集成轮子

--- app/src/main/java/com/homechart/app/home/activity/ShouCangListActivity.java
+++ app/src/main/java/com/homechart/app/home/activity/ShouCangListActivity.java
@@ -1,20 +1,24 @@
 package com.homechart.app.home.activity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.homechart.app.R;
 import com.homechart.app.home.base.BaseActivity;
+import com.homechart.app.timepiker.citypickerview.widget.CityPicker;
 
 /**
  * Created by gumenghao on 17/6/7.
  */
 
-public class ShouCangListActivity extends BaseActivity implements View.OnClickListener{
+public class ShouCangListActivity extends BaseActivity implements View.OnClickListener {
     private ImageButton mIBBack;
     private TextView mTVTital;
+    private CityPicker cityPicker;
 
     @Override
     protected int getLayoutResId() {
@@ -38,14 +42,44 @@ public class ShouCangListActivity extends BaseActivity implements View.OnClickLi
     @Override
     protected void initData(Bundle savedInstanceState) {
         mTVTital.setText("收藏");
+
+        cityPicker = new CityPicker.Builder(ShouCangListActivity.this).textSize(20)
+                .titleTextColor("#000000")
+                .backgroundPop(R.color.white)
+                .province("北京市")
+                .city("北京市")
+                .textColor(Color.parseColor("#000000"))
+                .provinceCyclic(true)
+                .cityCyclic(false)
+                .districtCyclic(false)
+                .visibleItemsCount(7)
+                .itemPadding(10)
+                .onlyShowProvinceAndCity(true)
+                .build();
+
+
     }
 
     @Override
     public void onClick(View v) {
 
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.nav_left_imageButton:
-                ShouCangListActivity.this.finish();
+                cityPicker.show();
+                cityPicker.setOnCityItemClickListener(new CityPicker.OnCityItemClickListener() {
+                    @Override
+                    public void onSelected(String... citySelected) {
+
+                        Toast.makeText(ShouCangListActivity.this, "选择结果：\n省：" + citySelected[0] + "\n市：" + citySelected[1] + "\n区："
+                                + citySelected[2] + "\n邮编：" + citySelected[3], Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Toast.makeText(ShouCangListActivity.this, "已取消", Toast.LENGTH_LONG).show();
+                    }
+                });
+//                ShouCangListActivity.this.finish();
                 break;
         }
     }
