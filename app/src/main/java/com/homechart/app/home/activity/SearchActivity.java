cd38c388fc58286c5f2c0f2c097f69c974f6406e
gumenghao <menghao.gu@leediancn.com>
2017-06-13 18:38:20 +0800

    添加自动换行的控件，完美实现

--- app/src/main/java/com/homechart/app/home/activity/SearchActivity.java
+++ app/src/main/java/com/homechart/app/home/activity/SearchActivity.java
@@ -6,6 +6,8 @@ import android.widget.TextView;
 
 import com.homechart.app.R;
 import com.homechart.app.home.base.BaseActivity;
+import com.homechart.app.myview.WrapLayout;
+import com.homechart.app.utils.ToastUtils;
 
 /**
  * Created by gumenghao on 17/6/13.
@@ -15,6 +17,11 @@ public class SearchActivity
         extends BaseActivity
         implements View.OnClickListener {
     private TextView tv_quxiao;
+    private WrapLayout wl_tips;
+    private String[] myData = new String[]
+            {"1", "saasas2", "3", "4", "5", "6", "7",
+                    "8", "9", "10", "11", "12", "13", "14",
+                    "15", "16", "17", "18", "19", "20", "21"};
 
     @Override
     protected int getLayoutResId() {
@@ -25,17 +32,25 @@ public class SearchActivity
     protected void initView() {
 
         tv_quxiao = (TextView) findViewById(R.id.tv_quxiao);
+        wl_tips = (WrapLayout) findViewById(R.id.wl_tips);
     }
 
     @Override
     protected void initData(Bundle savedInstanceState) {
-
+        wl_tips.setStyle(0);
+        wl_tips.setData(myData, this, 15, 10, 10, 10, 10, 10, 10, 10, 10);
     }
 
     @Override
     protected void initListener() {
         super.initListener();
         tv_quxiao.setOnClickListener(this);
+        wl_tips.setMarkClickListener(new WrapLayout.MarkClickListener() {
+            @Override
+            public void clickMark(int position) {
+                ToastUtils.showCenter(SearchActivity.this, myData[position]);
+            }
+        });
     }
 
     @Override
