d2e3df244ef06ac0ebcd9653e71e33b13303cff8
gumenghao <menghao.gu@leediancn.com>
2017-06-15 18:40:38 +0800

    调试推荐关键tag词语

--- app/src/main/java/com/homechart/app/home/activity/ShaiXuanResultActicity.java
+++ app/src/main/java/com/homechart/app/home/activity/ShaiXuanResultActicity.java
@@ -1,10 +1,12 @@
 package com.homechart.app.home.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.android.volley.VolleyError;
@@ -13,8 +15,10 @@ import com.homechart.app.commont.ClassConstant;
 import com.homechart.app.home.base.BaseActivity;
 import com.homechart.app.home.bean.pictag.TagDataBean;
 import com.homechart.app.home.bean.shaixuan.ShaiXuanBean;
+import com.homechart.app.myview.WrapLayout;
 import com.homechart.app.utils.GsonUtil;
 import com.homechart.app.utils.ToastUtils;
+import com.homechart.app.utils.UIUtils;
 import com.homechart.app.utils.volley.MyHttpManager;
 import com.homechart.app.utils.volley.OkStringRequest;
 
@@ -33,6 +37,8 @@ public class ShaiXuanResultActicity
     private String shaixuan_tag;
     private ImageButton nav_left_imageButton;
     private TextView tv_tital_comment;
+    private WrapLayout wl_tips;
+    private String[] myData;
 
     @Override
     protected int getLayoutResId() {
@@ -52,6 +58,7 @@ public class ShaiXuanResultActicity
 
         nav_left_imageButton = (ImageButton) findViewById(R.id.nav_left_imageButton);
         tv_tital_comment = (TextView) findViewById(R.id.tv_tital_comment);
+        wl_tips = (WrapLayout) findViewById(R.id.wl_tips);
 
     }
 
@@ -59,6 +66,14 @@ public class ShaiXuanResultActicity
     protected void initListener() {
         super.initListener();
         nav_left_imageButton.setOnClickListener(this);
+        wl_tips.setMarkClickListener(new WrapLayout.MarkClickListener() {
+            @Override
+            public void clickMark(int position) {
+
+                // 跳转搜索结果页
+
+            }
+        });
     }
 
     @Override
@@ -125,11 +140,34 @@ public class ShaiXuanResultActicity
             }
         }
     };
-
+    /**
+     * 设置数据
+     *
+     * @param data     文字
+     * @param context  上下文
+     * @param textSize 文字大小
+     * @param pl       左内边距
+     * @param pt       上内边距
+     * @param pr       右内边距
+     * @param pb       下内边距
+     * @param ml       左外边距
+     * @param mt       上外边距
+     * @param mr       右外边距
+     * @param mb       下外边距
+     */
     private void changeUI() {
 
         if (null != strTuiJian && strTuiJian.size() > 0) {
+            wl_tips.setVisibility(View.VISIBLE);
+            myData = new String[strTuiJian.size()];
+            for (int i = 0; i < strTuiJian.size(); i++) {
+                myData[i] = strTuiJian.get(i);
+            }
+            wl_tips.setStyle(0);
+            wl_tips.setData(myData, ShaiXuanResultActicity.this, 14, 15, 10, 15, 10, 0, 0, UIUtils.getDimens(R.dimen.font_12), UIUtils.getDimens(R.dimen.font_15));
 
+        } else {
+            wl_tips.setVisibility(View.GONE);
         }
 
 
