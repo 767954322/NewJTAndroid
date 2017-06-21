331ae6c11914cab150c87a71c0ee3e63bf42f4b8
gumenghao <menghao.gu@leediancn.com>
2017-06-21 14:39:42 +0800

    修改布局

--- app/src/main/java/com/homechart/app/home/activity/FaBuActvity.java
+++ app/src/main/java/com/homechart/app/home/activity/FaBuActvity.java
@@ -1,7 +1,10 @@
 package com.homechart.app.home.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.homechart.app.R;
 import com.homechart.app.home.base.BaseActivity;
@@ -13,9 +16,14 @@ import com.umeng.socialize.media.Base;
  * Created by gumenghao on 17/6/21.
  */
 
-public class FaBuActvity extends BaseActivity {
-    private RoundImageView image;
+public class FaBuActvity
+        extends BaseActivity
+        implements View.OnClickListener {
+    private ImageView iv_image_fabu;
     private String urlImage;
+    private ImageButton nav_left_imageButton;
+    private TextView tv_tital_comment;
+    private TextView tv_content_right;
 
     @Override
     protected int getLayoutResId() {
@@ -31,13 +39,36 @@ public class FaBuActvity extends BaseActivity {
     @Override
     protected void initView() {
 
-        image = (RoundImageView) findViewById(R.id.image);
+        nav_left_imageButton = (ImageButton) findViewById(R.id.nav_left_imageButton);
+        tv_tital_comment = (TextView) findViewById(R.id.tv_tital_comment);
+        tv_content_right = (TextView) findViewById(R.id.tv_content_right);
+        iv_image_fabu = (ImageView) findViewById(R.id.iv_image_fabu);
+    }
+
+    @Override
+    protected void initListener() {
+        super.initListener();
+        nav_left_imageButton.setOnClickListener(this);
     }
 
     @Override
     protected void initData(Bundle savedInstanceState) {
+        tv_tital_comment.setText("发布图片");
+        tv_content_right.setText("发布");
+        ImageUtils.displayFilletImage("file://" + urlImage, iv_image_fabu);
 
-        ImageUtils.displayRoundImage("file://" + urlImage, image);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.nav_left_imageButton:
+                FaBuActvity.this.finish();
+                break;
+            case R.id.tv_content_right:
+
+        }
 
     }
 }
