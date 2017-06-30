bcbb733df488743aa90cb83398c5aa2a5a4ab805
gumenghao <menghao.gu@leediancn.com>
2017-06-30 14:42:52 +0800

    修改pop样式及活动时间几种状态

--- app/src/main/java/com/homechart/app/myview/HomeActivityPopWin.java
+++ app/src/main/java/com/homechart/app/myview/HomeActivityPopWin.java
@@ -11,12 +11,16 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.homechart.app.R;
+import com.homechart.app.commont.PublicUtils;
 import com.homechart.app.home.adapter.HomeTagAdapter;
 import com.homechart.app.home.bean.fabu.ActivityItemDataBean;
 import com.homechart.app.home.bean.pictag.TagDataBean;
 import com.homechart.app.home.bean.pictag.TagItemDataBean;
+import com.homechart.app.utils.UIUtils;
 import com.homechart.app.utils.imageloader.ImageUtils;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class HomeActivityPopWin extends PopupWindow {
@@ -25,9 +29,10 @@ public class HomeActivityPopWin extends PopupWindow {
     private final View view_below;
     private final ImageView iv_activity_image;
     private final TextView tv_activity_image_tital;
-    private final TextView tv_activity_time_start;
     private final TextView tv_activity_detail_content;
-    private final TextView tv_activity_time_end;
+    private final RelativeLayout rl_data_last;
+    private final TextView tv_data_last;
+    private final ImageView iv_data_last_icon;
     private View view;
     private Context mContext;
     private List<TagItemDataBean> mTagList;
@@ -41,16 +46,25 @@ public class HomeActivityPopWin extends PopupWindow {
     private void changeUI() {
 
         if (mActivityItemDataBean != null) {
-            String startTime = mActivityItemDataBean.getActivity_info().getStart_time();
-            String endTime = mActivityItemDataBean.getActivity_info().getEnd_time();
-            String[] start = startTime.split(" ");
-            String[] end = endTime.split(" ");
+            if (mActivityItemDataBean.getActivity_info().getState_id().equals("1")) {//未开始
 
+            } else if (mActivityItemDataBean.getActivity_info().getState_id().equals("2")) {//已结束
+                iv_data_last_icon.setImageResource(R.drawable.shijian1);
+                tv_data_last.setText("已结束");
+                tv_data_last.setTextColor(UIUtils.getColor(R.color.bg_8f8f8f));
+            } else if (mActivityItemDataBean.getActivity_info().getState_id().equals("3")) {//进行中
+                iv_data_last_icon.setImageResource(R.drawable.shijian);
+                tv_data_last.setTextColor(UIUtils.getColor(R.color.bg_e79056));
+                //计算时间
+                SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                Date curDate = new Date(System.currentTimeMillis());
+                String str = formatter.format(curDate);
+                long data = PublicUtils.diffDay(mActivityItemDataBean.getActivity_info().getEnd_time(), str, "yyyy-MM-dd HH:mm:ss");
+                tv_data_last.setText("还剩" + data + "天");
+            }
             ImageUtils.displayFilletImage(mActivityItemDataBean.getActivity_info().getImage().getImg0(), iv_activity_image);
             tv_activity_image_tital.setText(mActivityItemDataBean.getActivity_info().getTitle());
-            tv_activity_time_start.setText(start[0].replace("-","/") + "-");
-            tv_activity_time_end.setText(end[0].replace("-","/"));
-            tv_activity_detail_content.setText(mActivityItemDataBean.getActivity_info().getDescription());
+            tv_activity_detail_content.setText("活动介绍：" + mActivityItemDataBean.getActivity_info().getDescription());
         }
 
     }
@@ -60,13 +74,14 @@ public class HomeActivityPopWin extends PopupWindow {
         this.mContext = context;
         this.view = LayoutInflater.from(context).inflate(R.layout.wk_activity_popwindow, null);
 
+        rl_data_last = (RelativeLayout) view.findViewById(R.id.rl_data_last);
+        tv_data_last = (TextView) view.findViewById(R.id.tv_data_last);
         view_up = view.findViewById(R.id.view_up);
         view_below = view.findViewById(R.id.view_below);
         iv_activity_image = (ImageView) view.findViewById(R.id.iv_activity_image);
         tv_activity_image_tital = (TextView) view.findViewById(R.id.tv_activity_image_tital);
-        tv_activity_time_start = (TextView) view.findViewById(R.id.tv_activity_time_start);
-        tv_activity_time_end = (TextView) view.findViewById(R.id.tv_activity_time_end);
         tv_activity_detail_content = (TextView) view.findViewById(R.id.tv_activity_detail_content);
+        iv_data_last_icon = (ImageView) view.findViewById(R.id.iv_data_last_icon);
 
 
         view_up.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/homechart/app/myview/HomeActivityPopWinEdit.java
+++ app/src/main/java/com/homechart/app/myview/HomeActivityPopWinEdit.java
@@ -10,11 +10,15 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.homechart.app.R;
+import com.homechart.app.commont.PublicUtils;
 import com.homechart.app.home.bean.fabu.ActivityItemDataBean;
 import com.homechart.app.home.bean.fabu.ActivityItemInfoDataBean;
 import com.homechart.app.home.bean.pictag.TagItemDataBean;
+import com.homechart.app.utils.UIUtils;
 import com.homechart.app.utils.imageloader.ImageUtils;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class HomeActivityPopWinEdit extends PopupWindow {
@@ -23,9 +27,10 @@ public class HomeActivityPopWinEdit extends PopupWindow {
     private final View view_below;
     private final ImageView iv_activity_image;
     private final TextView tv_activity_image_tital;
-    private final TextView tv_activity_time_start;
     private final TextView tv_activity_detail_content;
-    private final TextView tv_activity_time_end;
+    private final RelativeLayout rl_data_last;
+    private final TextView tv_data_last;
+    private final ImageView iv_data_last_icon;
     private View view;
     private Context mContext;
     private List<TagItemDataBean> mTagList;
@@ -39,16 +44,25 @@ public class HomeActivityPopWinEdit extends PopupWindow {
     private void changeUI() {
 
         if (activityItemInfoDataBean != null) {
-            String startTime = activityItemInfoDataBean.getStart_time();
-            String endTime = activityItemInfoDataBean.getEnd_time();
-            String[] start = startTime.split(" ");
-            String[] end = endTime.split(" ");
+            if (activityItemInfoDataBean.getState_id().equals("1")) {//未开始
 
+            } else if (activityItemInfoDataBean.getState_id().equals("2")) {//已结束
+                iv_data_last_icon.setImageResource(R.drawable.shijian1);
+                tv_data_last.setText("已结束");
+                tv_data_last.setTextColor(UIUtils.getColor(R.color.bg_8f8f8f));
+            } else if (activityItemInfoDataBean.getState_id().equals("3")) {//进行中
+                iv_data_last_icon.setImageResource(R.drawable.shijian);
+                tv_data_last.setTextColor(UIUtils.getColor(R.color.bg_e79056));
+                //计算时间
+                SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                Date curDate = new Date(System.currentTimeMillis());
+                String str = formatter.format(curDate);
+                long data = PublicUtils.diffDay(activityItemInfoDataBean.getEnd_time(), str, "yyyy-MM-dd HH:mm:ss");
+                tv_data_last.setText("还剩" + data + "天");
+            }
             ImageUtils.displayFilletImage(activityItemInfoDataBean.getImage().getImg0(), iv_activity_image);
             tv_activity_image_tital.setText(activityItemInfoDataBean.getTitle());
-            tv_activity_time_start.setText(start[0].replace("-","/") + "-");
-            tv_activity_time_end.setText(end[0].replace("-","/"));
-            tv_activity_detail_content.setText(activityItemInfoDataBean.getDescription());
+            tv_activity_detail_content.setText("活动介绍："+activityItemInfoDataBean.getDescription());
         }
 
     }
@@ -58,13 +72,14 @@ public class HomeActivityPopWinEdit extends PopupWindow {
         this.mContext = context;
         this.view = LayoutInflater.from(context).inflate(R.layout.wk_activity_popwindow, null);
 
+        rl_data_last = (RelativeLayout) view.findViewById(R.id.rl_data_last);
+        tv_data_last = (TextView) view.findViewById(R.id.tv_data_last);
         view_up = view.findViewById(R.id.view_up);
         view_below = view.findViewById(R.id.view_below);
         iv_activity_image = (ImageView) view.findViewById(R.id.iv_activity_image);
         tv_activity_image_tital = (TextView) view.findViewById(R.id.tv_activity_image_tital);
-        tv_activity_time_start = (TextView) view.findViewById(R.id.tv_activity_time_start);
-        tv_activity_time_end = (TextView) view.findViewById(R.id.tv_activity_time_end);
         tv_activity_detail_content = (TextView) view.findViewById(R.id.tv_activity_detail_content);
+        iv_data_last_icon = (ImageView) view.findViewById(R.id.iv_data_last_icon);
 
 
         view_up.setOnClickListener(new View.OnClickListener() {
