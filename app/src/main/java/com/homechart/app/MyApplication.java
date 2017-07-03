cce60f1ad28282a887e758d517375d6137b5a86d
gumenghao <menghao.gu@leediancn.com>
2017-07-03 18:45:35 +0800

    解决bug

--- app/src/main/java/com/homechart/app/MyApplication.java
+++ app/src/main/java/com/homechart/app/MyApplication.java
@@ -3,6 +3,8 @@ package com.homechart.app;
 import android.app.Application;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
+import android.os.StrictMode;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
@@ -35,6 +37,10 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+            StrictMode.setVmPolicy(builder.build());
+        }
         myApplication = this;
         queue = Volley.newRequestQueue(this);
         initImageLoader();
